e7a199e5d3792c3ef63b6211f190de9992b0e0f0
Luis Zapater <37409093+lzapater@users.noreply.github.com>
2021-06-08 12:27:33 -0500

    Update main.cpp

--- Clases18-8/main.cpp
+++ Clases18-8/main.cpp
@@ -2,59 +2,45 @@
 
 using namespace std;
 
-int main()
-{
-
-    int a=6,b=5;
-    int suma=a+b;
-    int multip=a*b;
-    float div=a/b;
-    cout <<"La suma es: "<<suma<<endl;
-    cout <<"La multplicacion es: "<<multip<<endl;
-    cout <<"La division es: "<<div;
-
-
-
-/*
-    int n1;
-    int n2;
-
-    cout<<"Ingrese un numero: ";
-    cin>>n1;
-    cout<<"Ingrese un numero: ";
-    cin>>n2;
-
-    int sum1=n1+n2;
-    int mult1=n1*n2;
-    float div1=n1/n2;
-
-    cout <<"La suma es: "<<sum1<<endl;
-    cout <<"La multplicacion es: "<<mult1<<endl;
-    cout <<"La division es: "<<div1;
-
-
-*/
-/*
-    int n3;
-    int n4;
-
-    cout<<"Ingrese un numero: ";
-    cin>>n3;
-    cout<<"Ingrese un numero: ";
-    cin>>n4;
-
-    if(n3>n4)
-        cout<<n3<<" es el mayor";
-
-    else if(n3==n4)
-            cout<<"Ambos numeros son iguales";
-
-    else
-        cout<<n4<<" es el mayor";
-
-*/
-
-
-return 0;
+template <typename Type> // Parámetro tipo template
+class Stack {
+  private:
+    Type *stackPtr;
+    int maxLen;
+    int topSub;
+    
+  public:
+    Stack() { //Constructo de 100 elementos
+    stackPtr = new Type [100];
+    maxLen = 99;
+    topSub = -1;
 }
 
+    Stack(int size) { // Constructor de n dados elementos
+      stackPtr = new Type [size];
+      maxLen = size - 1;
+      topSub = -1;
+}
+    
+    ~Stack() {delete stackPtr;}; // Destructor
+
+    void push(Type number) {
+      if (topSub == maxLen)
+        cout << "Error in push—stack is full\n";
+      else stackPtr[++ topSub] = number;
+}
+    void pop() {
+      if (empty())
+        cout << "Error in pop—stack is empty\n";
+      else topSub --;
+}
+
+Type top() {
+    if (empty())
+      cout << "Error in top--stack is empty\n";
+    else
+      return (stackPtr[topSub]);
+}
+    
+int empty() {return (topSub == -1);}
+}
